63ebe4c7b65191ad51c81e71d4a7b3d743519ee4
bmark88 <bradley.mark@hotmail.com>
2020-04-01 18:23:03 +0000

    added protection against SQL injection for student and teacher queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -12,17 +12,37 @@ pool.connect((err) => {
   console.log('connected');
 })
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+// UNSAFE CODE - UNPROTECTED FROM SQL INJECTION
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })
+// });
+
+
+// SAFE CODE - PROTECTED FROM SQL INJECTION
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+  `;
+
+const values = [process.argv[2]];
+
+pool
+  .query(queryString, values)
+  .then(res => console.log(res.rows))
+  .catch(e => console.error('query error', e.stack));
